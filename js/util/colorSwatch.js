3c78333e580a098e2d9efeaa58800a3c1b0aec9c
Amanda Tavares <amandacrisrodriguest@gmail.com>
2019-10-23 15:18:11 -0300

    Add: color swatch

---
+++ js/util/colorSwatch.js
@@ -0,0 +1,56 @@
+const SWATCH_LAVANDER = [
+    '#E2CFEA',
+    '#A06CD5',
+    '#6247AA',
+    '#102B3F',
+    '#062726'
+];
+
+const SWATCH_REDDISH = [
+    '#A7D49B',
+    '#92AC86',
+    '#696047',
+    '#55251D',
+    '#5A1807'
+];
+
+const SWATCH_GREYISH = [
+    '#848C8E',
+    '#435058',
+    '#DCF763',
+    '#BFB7B6',
+    '#F1F2EE'
+];
+
+const SWATCH_PURPLE = [
+    '#000505',
+    '#3B3355',
+    '#5D5D81',
+    '#BFCDE0',
+    '#FEFCCC'
+];
+
+const SWATCH_COLORFUL = [
+    '#E3B505',
+    '#3B3355',
+    '#95190C',
+    '#107E7D',
+    '#044B7F'
+];
+
+const SWATCH_POP = [
+    '#D8D8D8',
+    '#FFFFEA',
+    '#DA627D',
+    '#00CECB',
+    '#FFED66'
+];
+
+const GLOBAL_SWATCHES = [
+    SWATCH_LAVANDER,
+    SWATCH_REDDISH,
+    SWATCH_GREYISH,
+    SWATCH_PURPLE,
+    SWATCH_COLORFUL,
+    SWATCH_POP
+];
